4e954c4fa26372544d4b5044f9d1ba866f6c4a7b
Richard Burd <richardburdx@yahoo.com>
2017-11-30 17:03:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,10 +24,4 @@ class App < Sinatra::Base
     "#{@product}"
   end
 
-  get '/medicines/:id' do
-    @medicine = all_the_medicines.select do |medicine|
-      medicine.id == params[:id]
-    end.first
-  end
-
 end
